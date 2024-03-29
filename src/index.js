1a07395b67798c76b8e0487c3fc900cafe69b090
Deepak Hegde <deepakhegde@sketchbrahma.com>
2024-03-29 18:57:23 +0530

    white space issue resolved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SolarSystem from './containers/SolarSystem';
+import "./App.css"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
